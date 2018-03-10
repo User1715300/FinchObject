37f623fc7f724a61d933d42bdf0db4b39f43f34c
User1715300 <1715300@brunel.ac.uk>
2018-03-10 22:29:20 +0000

    22:29

--- src/FollowObject/Test.java
+++ src/FollowObject/Test.java
@@ -13,19 +13,20 @@ public class Test {
     static double averageSpeed = 0;
     static double speedAddedStops = 0;
     static double travelTime = 0;
-    static int stopCount = 0;
-    static double TotalStopStime = 0;
-    static int tapcount = 0; 						//Counts how many times finch is tapped
+    static int StopCount = 0;
+    static double StopTime = 0;
     static double TimeSeconds;
+    static double SleepTime;
     static int Sleep = 100;
+    static int tapcount = 2; 						//Counts how many times finch is tapped
     
     public static void main(String[] args) 
     {
-    	TimeSeconds = Duration/1000;
-    	//myF.setLED(255,168,0); 					//Test
+    	TimeSeconds = ((Duration + Sleep)/1000);
+    	SleepTime =  Sleep / 1000;
+/**/    myF.setLED(255,168,0); 						//Test
 /**/	System.out.println("Connected"); 			//Test 
-    	TurnSpeed = (((NormSpeed - Turn) / 2) + Turn) ;
-    	//System.out.println(TurnSpeed);
+    	TurnSpeed = (((NormSpeed - Turn) / 2) + Turn);
     	
     	while (myF.isFinchUpsideDown() == false){
     		if (tapcount == 0) {
@@ -33,19 +34,16 @@ public class Test {
     			if (myF.isTapped() == true) {
     				if (myF.isObstacleLeftSide() == true && myF.isObstacleRightSide() == true) {
     					tapcount = tapcount + 1;
-/**/    					System.out.print("Test"); //Test
-    				}
-    			}
-    		}
+/**/    				System.out.print("Test"); 	//Test
+    		}}}    		
     		if (tapcount > 0){
     			if (myF.isTapped() == true) {
     				tapcount = tapcount + 1;
-    			}
-    		}
-/**/			System.out.println(myF.isTapped());		//Test
-/**/			System.out.println(tapcount);			//Test
+    		}}
+/**/			System.out.println(myF.isTapped());	//Test
+/**/			System.out.println(tapcount);		//Test
 			if (tapcount == 1 || tapcount == 2) { 	
-/**/		            myF.setLED(0,0,255);				//Test				 
+/**/		            myF.setLED(0,0,255);		//Test				 
 		            Move();
 			}else if (tapcount == 3 || tapcount > 3){
 				myF.quit();							//Disconnects the finch
@@ -60,40 +58,44 @@ public class Test {
         System.out.println("y or n");
         Scanner input = new Scanner(System.in);
         String YorN = input.nextLine();
-        switch (YorN) { //allows the user to choose to view statistical results or not
+        switch (YorN) { 							//allows the user to choose to view statistical results or not
             case "y":
+            	
+            	double StopTimeRound = Math.round(StopTime * 100)/100.0;
+            	double AverageSpeedRound = Math.round(averageSpeed * 100)/100.0;
+            	
                 System.out.println("The available statistical results are average speed(1), total travel time(2), number of stops(3), and total time stopped(4).");
                 System.out.println("Please enter the indicated number in brackets to the statistic result, or enter 'all' to view all of them");
                 Scanner userChoice = new Scanner(System.in);
                 String StatSelect = input.nextLine();
-                switch (StatSelect) { //User picks stats to view
-                    case "1": 
-                        System.out.println("The average speed was " + (averageSpeed / speedAddedStops) + ".");
+                switch (StatSelect) { 				//User picks stats to view
+                    case "1": //averageSpeed
+                        System.out.println("The average speed was " + (AverageSpeedRound / speedAddedStops) + ".");
                         break;
                     case "2": 
                         System.out.println("The total travel time was " + travelTime + " seconds.");
                         break;
                     case "3": 
-                        System.out.println("The number of stops was " + stopCount + ".");
+                        System.out.println("The number of stops was " + StopCount + ".");
                         break;
                     case "4": 
-                        System.out.println("The total time stopped was " + TotalStopStime + " seconds.");
+                        System.out.println("The total time stopped was " + StopTime + " seconds.");
                         break;
                     case "all": 
-                        System.out.println("The average speed was " + (averageSpeed / speedAddedStops) + ".");
+                        System.out.println("The average speed was " + (AverageSpeedRound / speedAddedStops) + ".");
                         System.out.println("The total travel time was " + travelTime + " seconds.");
-                        System.out.println("The number of stops was " + stopCount + ".");
-                        System.out.println("The total time stopped was " + TotalStopStime + " seconds.");
-                        break;
+                        System.out.println("The number of stops was " + StopCount + ".");
+                        System.out.println("The total time stopped was " + StopTimeRound + " seconds.");
+                        break;						//exit
                     default:
-                        break; //exit
+                        break; 						//exit
                 }
                 break;
             case "n":
             	System.out.print("Thank you for using the finch, the program has now ended.");
-                System.exit(0);; //exit
+                System.exit(0); 					//exit
             default:
-                break; //exit
+                break; 								//exit
         }
     }
 
@@ -113,46 +115,46 @@ public class Test {
 
     public static void ONWARDS() 
     {
-        myF.setLED(0,255,0); 	//Green
+        myF.setLED(0,255,0); 						//Green
         myF.buzz(BuzzFeq, Duration); 
         myF.setWheelVelocities(NormSpeed, NormSpeed, Duration); 
         myF.sleep(Sleep);
-        speedAddedStops = speedAddedStops + 1.0; //keeps track of the speed calculation divider
-        averageSpeed = averageSpeed + NormSpeed; //adds to the average speed
-        travelTime = travelTime + 0.1; //adds to the time travelled counter
+        speedAddedStops = speedAddedStops + 1.0; 	//keeps track of the speed calculation divider
+        averageSpeed = averageSpeed + NormSpeed; 	//adds to the average speed
+        travelTime = travelTime + TimeSeconds; 		//adds to the time travelled counter
     }
 
-    public static void Hidari() //JPN L
+    public static void Hidari() 					//JPN L
     {
-        myF.setLED(0,255,0); 	//Green
+        myF.setLED(0,255,0); 						//Green
         myF.buzz(BuzzFeq, Duration); 
         myF.setWheelVelocities(Turn, NormSpeed, Duration); 
         myF.sleep(Sleep);
-        speedAddedStops = speedAddedStops + 1.0; //keeps track of the speed calculation divider
-        averageSpeed = averageSpeed + TurnSpeed; //adds to the average speed
-        travelTime = travelTime + 0.1; //adds one second to the time travelled counter
+        speedAddedStops = speedAddedStops + 1.0; 	//keeps track of the speed calculation divider
+        averageSpeed = averageSpeed + TurnSpeed; 	//adds to the average speed
+        travelTime = travelTime + TimeSeconds; 		//adds one second to the time travelled counter
     }
 
-    public static void Migi() //JPN R
+    public static void Migi() 						//JPN R
     {
-        myF.setLED(0,255,0); 	//Green
+        myF.setLED(0,255,0); 						//Green
         myF.buzz(BuzzFeq, Duration); 
         myF.setWheelVelocities(NormSpeed, Turn, Duration); 
         myF.sleep(Sleep);
-        speedAddedStops = speedAddedStops + 1.0; //keeps track of the speed calculation divider
-        averageSpeed = averageSpeed + TurnSpeed; //adds to the average speed
-        travelTime = travelTime + 0.1; //adds to the time travelled counter
+        speedAddedStops = speedAddedStops + 1.0; 	//keeps track of the speed calculation divider
+        averageSpeed = averageSpeed + TurnSpeed; 	//adds to the average speed
+        travelTime = travelTime + TimeSeconds; 		//adds to the time travelled counter
     }
 
     public static void CEASE() 
     {
-        myF.setLED(255,0,0); 	//Red
+        myF.setLED(255,0,0); 						//Red
         myF.buzz(0, Duration); 
         myF.stopWheels(); 
         myF.sleep(Sleep);
-        speedAddedStops = speedAddedStops + 1.0; //keeps track of the speed calculation divider
-        TotalStopStime = TotalStopStime + 0.1; //adds one second to the time stopped counter
-        stopCount++; //adds one to the amount of times stopped counter
+        speedAddedStops = speedAddedStops + 1.0; 	//keeps track of the speed calculation divider
+        StopTime = StopTime + 0.1;		 			//adds one second to the time stopped counter
+        StopCount = StopCount +1; 					//adds one to the amount of times stopped counter
     }
 	
 }
